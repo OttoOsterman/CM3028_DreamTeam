a23a158f798c882c04fe61b981c1e681f98cf855
Andrew <admac21@gmail.com>
2016-10-10 15:44:11 +0100

    push

--- index.php
+++ index.php
@@ -5,14 +5,21 @@
 <body>
 <p>
     <?php
-    $name = "Edgar";
-
-    if($name == "Simon"){
-        print "I know you";
+    $myage = 24;
+    $myname = "Andrew";
+    print "Hi " . $myname . "! You can buy: ";
+    if($myage > 16){
+        print "specs";
+    }
+    if($myage > 18 && $myage < 22){
+        print " and mugs.";
+    }if($myage > 21){
+        print ", mugs and sausage rolls.";
     }
     else{
-        print "Who are you?!";
+        print "Nothing, soz!";
     }
+
     ?>
 </p>
 </body>
