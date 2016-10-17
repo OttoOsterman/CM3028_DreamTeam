9d4996aabaf3e26e0b32ac99fd8a98e4e3da7b56
Andrew <admac21@gmail.com>
2016-10-17 14:15:40 +0100

    HelloSelector

--- helloSelector.php
+++ helloSelector.php
@@ -3,10 +3,14 @@
 <head>
 </head>
 <body>
+<form action="submitform.php" method ="post">
+    <input type="submit" value="Earth">
+</form>
 <p>
     <?php
 
     echo "Where would you like to say hello?";
+
 
     ?>
 </p>
