0ad44c0b4a82f929ca78c4efc56e21ccd7ba851c
OttoOsterman <ottosterman@gmail.com>
2016-10-31 14:21:08 +0000

    added php things

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-
+<!---->
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--<head>-->
+<!--</head>-->
+<!--<body>-->
+<!--<p>-->
+//
+//    include("dbconnect.php");
+//
+//    //Connect to csdm-webserver and select database
+//    $db = new mysqli(
+//        'Us-cdbr-azure-southcentral-f.cloudapp.net',
+//        'Be9b6ca39cad35',
+//        'e7f30e0c',
+//        'cm3028dreamteam'
+//    );
+//    //Test if connection was established, and print any errors
+//    if($db->connect_errno){
+//        die('Connectfailed['.$db->connect_error.']');
+//    }
+//    echo "It's alive!";
+//
+//    ?>
+<!--</p>-->
+<!--</body>-->
+<!--</html>-->
 <?
 define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
 $rules = array(
