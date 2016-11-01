4b795dd8114e0c6424155c259dba6012069e7e06
Jack Neilson <jackdneilson@gmail.com>
2016-11-01 15:29:24 +0000

    Added web.config and index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?
+
+define('INCLUDE_DIR', dirname(__FILE__) . '/src');
+
+$rules = array(
+		//TODO: REMOVE TESTING CODE
+		'database' => "/adminer"
+);
+
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+
+foreach($rules as $action => $rule) {
+	if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+		include(INCLUDE_DIR . $action . '.php');
+		exit();
+	}
+}
+
+include(INCLUDE_DIR . '404.php');
+
+?>
