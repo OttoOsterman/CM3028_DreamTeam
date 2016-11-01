0011b4ef6c19bbf7c1442eab75a6de3f80f180a6
Jack Neilson <jackdneilson@gmail.com>
2016-11-01 20:48:05 +0000

    Added homepage and navbar php and css

---
+++ src/scripts/navbar.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+	<div id="navbar">
+	<ul id="navbarlist">
+		<li><a href="http://go-portlethen.azurewebsites.net/">Home</a></li>
+		<li><a href="http://go-portlethen.azurewebsites.net/clubs">Clubs</a></li>
+		<li><a href="http://go-portlethen.azurewebsites.net/health_wellbeing">Health and Wellbeing</a></li>
+		<li><a href="http://go-portlethen.azurewebsites.net/map">Map</a></li>
+	</ul>
+';
+?>
