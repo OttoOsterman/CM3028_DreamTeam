f576ad57d2360321829257fd702e886c0beb5a8a
Jack Neilson <jackdneilson@gmail.com>
2016-11-07 14:46:19 +0000

    Added beginnings of map object

--- src/map.php
+++ src/map.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <head>
+	<title>Maps</title>
 	<link rel="stylesheet" type="text/css" href="./src/css/navbar.css"/>
 	<link rel="stylesheet" type="text/css" href="./src/css/general.css"/>
 	<link rel="stylesheet" type="text/css" href="./src/css/map.css"/>
+	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY" type="text/JavaScript"></script>
+	
+	<script type="text/JavaScript">
+	function load() {
+   		var map = new google.maps.Map(document.getElementById("map"), {
+ 	      	center: new google.maps.LatLng(47.6145, -122.3418),
+    	   	zoom: 13,
+        	mapTypeId: 'roadmap'
+      	});
+	}
+	</script>
 </head>
 
-<body>
+<body onload="load()">
 <?php include('scripts/navbar.php'); ?>
 
+<div id="map" style="width: 500px; height: 300px"></div>
 </body>
+</html>
