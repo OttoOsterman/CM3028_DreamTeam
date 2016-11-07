159ef798035c3f9c799a59cfb128b7d6e658cbe4
OttoOsterman <ottosterman@gmail.com>
2016-11-07 14:03:19 +0000

    lol

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -6,6 +6,7 @@ echo('
 		<li><a href="http://go-portlethen.azurewebsites.net/clubs">Clubs</a></li>
 		<li><a href="http://go-portlethen.azurewebsites.net/health_wellbeing">Health and Wellbeing</a></li>
 		<li><a href="http://go-portlethen.azurewebsites.net/map">Map</a></li>
+		<li><a class="button" href="https://youtu.be/dQw4w9WgXcQ">Click me</a></li>
 		');
 if (isset($_SESSION[username])) {
 	echo('<li><a id="logout_button" href="http://go-portlethen.azurewebsites.net/logout">Log out</a></li>');
