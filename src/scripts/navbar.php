248482c9fad46290013576a4d2a6e2e518baaf95
Jack Neilson <jackdneilson@gmail.com>
2016-11-07 14:14:44 +0000

    Made login/out buttons not list items

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -6,15 +6,13 @@ echo('
 		<li><a href="http://go-portlethen.azurewebsites.net/clubs">Clubs</a></li>
 		<li><a href="http://go-portlethen.azurewebsites.net/health_wellbeing">Health and Wellbeing</a></li>
 		<li><a href="http://go-portlethen.azurewebsites.net/map">Map</a></li>
+		</ul>
+	</div>
 		');
 if (isset($_SESSION[username])) {
-	echo('<li><a id="logout_button" href="http://go-portlethen.azurewebsites.net/logout">Log out</a></li>');
+	echo('<a id="logout_button" href="http://go-portlethen.azurewebsites.net/logout">Log out</a>');
 }
 else {
-	echo('<li><a id="login_button" href="http://go-portlethen.azurewebsites.net/login">Log in</a></li>');
+	echo('<a id="login_button" href="http://go-portlethen.azurewebsites.net/login">Log in</a>');
 }
-echo('
-	</ul>
-	</div>	
-		');
 ?>
