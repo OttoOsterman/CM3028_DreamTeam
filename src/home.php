72acec3282575f9926ba1d88ff807190d7e62409
Jack Neilson <jackdneilson@gmail.com>
2016-11-07 11:36:43 +0000

    Tried to get correct href for home css

--- src/home.php
+++ src/home.php
@@ -3,13 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="css/home.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo($_SERVER['REQUEST_URI']); ?>/css/home.css"/>
 	<link rel="stylesheet" type="text/css" href="css/navbar.css"/>
 </head>
 
 <body>
 	<div id="navbar">
-		<?php include 'scripts/navbar.php' ?>
+		<?php include('scripts/navbar.php') ?>
 	</div>
 	<div id="welcome">
 		<h1>
