0add27a3e87c482ad1f48c867be2be10f3603960
Jack Neilson <jackdneilson@gmail.com>
2016-11-07 12:09:56 +0000

    More changes to navbar css

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -2,10 +2,10 @@
 echo('
 	<div id="navbar">
 	<ul id="navbarlist">
-		<li><a href="http://go-portlethen.azurewebsites.net/">Home</a></li>
-		<li><a href="http://go-portlethen.azurewebsites.net/clubs">Clubs</a></li>
-		<li><a href="http://go-portlethen.azurewebsites.net/health_wellbeing">Health and Wellbeing</a></li>
-		<li><a href="http://go-portlethen.azurewebsites.net/map">Map</a></li>
+		<li><a class="button" href="http://go-portlethen.azurewebsites.net/">Home</a></li>
+		<li><a class="button" href="http://go-portlethen.azurewebsites.net/clubs">Clubs</a></li>
+		<li><a class="button" href="http://go-portlethen.azurewebsites.net/health_wellbeing">Health and Wellbeing</a></li>
+		<li><a class="button" href="http://go-portlethen.azurewebsites.net/map">Map</a></li>
 	</ul>
 	</div>
 ');
