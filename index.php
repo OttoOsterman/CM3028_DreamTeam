e780267e36d614cbe701ce9cde245d287bc05476
Jack Neilson <jackdneilson@gmail.com>
2016-11-07 10:36:48 +0000

    Removed testing code

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ $uri = urldecode($uri);
 
 foreach($rules as $action => $rule) {
 	if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
-		echo("" . INCLUDE_DIR . $action);
 		include(INCLUDE_DIR . $action . '.php');
 		exit();
 	}
