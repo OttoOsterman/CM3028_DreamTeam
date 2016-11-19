c0a7d3fa08eef71fb94b96bd3488cd7d81146bb4
Jack Neilson <jackdneilson@gmail.com>
2016-11-19 18:55:12 +0000

    Fixed links

--- src/clubs_societies.php
+++ src/clubs_societies.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/navbar.css"/>
+<title>Clubs</title>
+<link rel="stylesheet" type="text/css" href="./src/css/navbar.css"/>
+<link rel="stylesheet" type="text/css" href="./src/css/general.css"/>
+<link rel="stylesheet" type="text/css" href="./src/css/clubs.css"/>
 </head>
-<body>	
-	<?php include('scripts/navbar.php'); ?>
+<body>
+<?php include('scripts/header.php') ?>
 </body>
--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -3,7 +3,7 @@ echo('
 	<div id="navbar">
 	<ul id="navbarlist">
 		<li><a href="http://go-portlethen.azurewebsites.net/">Home</a></li>
-		<li><a href="http://go-portlethen.azurewebsites.net/clubs">Clubs</a></li>
+		<li><a href="http://go-portlethen.azurewebsites.net/clubs_societies">Clubs & Societies</a></li>
 		<li><a href="http://go-portlethen.azurewebsites.net/health_wellbeing">Health and Wellbeing</a></li>
 		<li><a href="http://go-portlethen.azurewebsites.net/map">Map</a></li>
 	</ul>
