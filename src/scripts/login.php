ca504031c24c71424fbc5a6d1d9978747defe94e
Jack Neilson <jackdneilson@gmail.com>
2016-11-21 15:27:52 +0000

    Added login logic

--- src/scripts/login.php
+++ src/scripts/login.php
@@ -1,2 +1,16 @@
 <?php
+//TODO: Remove testing code
+include("scripts/db_connect_test.php");
+$username = mysqli_real_escape_string($_POST["username"]);
+$password = $_POST["password"];
+
+$sql = "SELECT hash, salt FROM User WHERE username = {$username}";
+$result = $db->query($sql);
+if (mysql_num_rows($result) != 0) {
+    $hash = mysqli_fetch_array($result)[0];
+    $hashed_password = hash("sha256", $hashed_password);
+    if ($hashed_password === $hash) {
+        $_SESSION["username"] = $username;
+    }
+}
 ?>
