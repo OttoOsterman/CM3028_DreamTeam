8bd8f18170b14d3ebe2a98058b687d0c919dbbc1
Jack Neilson <jackdneilson@gmail.com>
2016-11-21 14:45:07 +0000

    Removed href from login button

--- src/scripts/login.php
+++ src/scripts/login.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1506801
- * Date: 21/11/2016
- * Time: 13:58
- */
+?>
--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -1,5 +1,6 @@
 <?php
 echo('
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<div id="navbar">
 	<ul id="navbarlist">
 		<li><a href="http://go-portlethen.azurewebsites.net/">Home</a></li>
@@ -9,10 +10,37 @@ echo('
 	</ul>
 		');
 if (isset($_SESSION[username])) {
-	echo('<a id="logout_button" href="http://go-portlethen.azurewebsites.net/logout">Log out</a>');
+	echo('
+		<a id="logout_button" onclick="logout()" href="http://go-portlethen.azurewebsites.net/logout">Log out</a>
+		<script type="text/javascript">
+			function logout() {
+				$.post("srcipts/logout.php", function() {
+					location.reload();
+				});
+			}
+		</script>
+	');
 }
 else {
-	echo('<a id="login_button" href="http://go-portlethen.azurewebsites.net/login">Log in</a>');
+	echo('
+			<script type="text/javascript">
+				function login_popup() {
+					$.getElementById("login_form").style.display = "block";
+					$.getElementById("cancel_button").addEventListener("click", function() {
+						$.getElementById("login_form").style.display = "none";
+					});
+				}
+			</script>
+			<a id="login_button" onclick="login_popup()">Log in</a>
+			<form id="login_form" style="display: none" action="scripts/login.php">
+				<label><b>Username: </b></label>
+				<input type="text" name="username" required>
+				<label><b>Password: </b></label>
+				<input type="password" name="password" required>
+				<button type="submit">Login</button>
+				<button type="cancelbtn" id="cancel_button">Cancel</button>
+			</form>
+		');
 }
 echo('</div>');
 ?>
