93e695639ed91c5018f703317deeb74db60d0653
Jack Neilson <jackdneilson@gmail.com>
2016-11-28 14:32:01 +0000

    Added auto_increment to SQL statements

--- src/scripts/db_connect_test.php
+++ src/scripts/db_connect_test.php
@@ -3,7 +3,7 @@ $db = new mysqli(
 		"us-cdbr-azure-southcentral-f.cloudapp.net",
 		"b2274755cf7719",
 		"3d705615",
-		"staging"
+		"acsm_c7b68337b4bae6d"
 		);
 if($db->connect_errno) {
 	die('Connectfailed['.$db->connect_error.']');
