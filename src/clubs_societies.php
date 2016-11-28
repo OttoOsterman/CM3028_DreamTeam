93814a2a8413d3f0d13e2779837516d0b933a744
Jack Neilson <jackdneilson@gmail.com>
2016-11-28 15:42:51 +0000

    Removed placeholder code

--- src/clubs_societies.php
+++ src/clubs_societies.php
@@ -1,21 +1,3 @@
-<?php
-echo('
-		<!DOCTYPE html>
-		<head>
-		<link rel="stylesheet" type="text/css" href="css/navbar.css"/>
-		</head>
-		<body>
-		
-');
-
-include('scripts/navbar.php');
-
-echo('
-		</body>
-		');
-?>
-
-//TODO: Fix club pages
 <!DOCTYPE html>
 <head>
 <title>Clubs</title>
