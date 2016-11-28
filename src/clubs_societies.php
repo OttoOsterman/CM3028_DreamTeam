f54eb4bc226304cdf293c4fe4aacf430ea775a2b
Jack Neilson <jackdneilson@gmail.com>
2016-11-28 15:48:27 +0000

    Fixed SQL select statement

--- src/clubs_societies.php
+++ src/clubs_societies.php
@@ -11,20 +11,19 @@
 <?php include('scripts/navbar.php') ?>
 <div id=clublist>
 	<?php
-	$sql = "SELECT Club.id, Club.name, Club.genre, Club.description, Photo.photo_path FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id";
+	$sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id";
 	$result = $db->query($sql);
 
 	while ($row = $result->fetch_array()) {
         if (isset($row['photo_path'])) {
             echo("
-			<img src={$row['photo_path']}><h1 id='clubName'><a href='club.php?club_id={$row["id"]}'>{$row['name']}</a></h1>" . "<div id='clubGenre'>{$row['genre']}</div>"
-                . "<div id='clubDesc'>{$row['description']}</div>"
-            );
+			<img src={$row['photo_path']}><h1 id='clubName'><a href='club.php?club_id={$row['id']}'>{$row['name']}</a></h1><div id='clubGenre'>{$row['genre']}</div><div id='clubDesc'>{$row['description']}</div>
+			");
         }
         else {
             echo("
-			<img src='images/default.jpg'><h1><a href='club.php?club_id={$row['id']}'>{$row['name']}</a></h1>" . $row['genre'] . $row['description']
-            );
+			<img src='images/default.jpg'><h1><a href='club.php?club_id={$row['id']}'>{$row['name']}</a></h1>{$row['genre']}{$row['description']}
+            ");
         }
 	}
 	?>
