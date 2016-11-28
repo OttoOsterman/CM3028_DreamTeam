e2e47938aa4d7180cb054dd9b058fbb64c1a20c5
Jack Neilson <jackdneilson@gmail.com>
2016-11-28 16:04:57 +0000

    Added testing code for index.php

--- index.php
+++ index.php
@@ -22,10 +22,9 @@ $uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
 $uri = urldecode($uri);
 
 foreach($rules as $action => $rule) {
-    var_dump($_SERVER['REQUEST_URI']);
 	if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
         echo(INCLUDE_DIR . $action . '.php' . $params);
-		include(INCLUDE_DIR . $action . '.php' . $params);
+		include(INCLUDE_DIR . $action . '.php' . $params[0]);
 		exit();
 	}
 }
