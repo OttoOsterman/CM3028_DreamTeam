a626fb089117adc770ae67e0f1b29a4b0fa997e6
OttoOsterman <ottosterman@gmail.com>
2016-11-28 11:45:18 +0000

    Added code for clubs and societies page + css

--- src/clubs_societies.php
+++ src/clubs_societies.php
@@ -5,6 +5,36 @@ echo('
 		<link rel="stylesheet" type="text/css" href="css/navbar.css"/>
 		</head>
 		<body>
+		
+		<div class="flex-container">
+<header>
+  <h1>Clubs and Societies</h1>
+</header>
+
+<nav class="nav">
+<ul>
+  <li><a href="#">Football</a></li>
+  <li><a href="#">Ice Hockey</a></li>
+  <li><a href="#">Tennis</a></li>
+</ul>
+</nav>
+
+<article class="article">
+  <h1>Football</h1>
+  <p>Spicy jalapeno bacon ipsum dolor amet t-bone jerky doner picanha. Spare ribs cupim tenderloin venison.
+   Ground round kevin landjaeger, sausage filet mignon turducken pig salami burgdoggen alcatra chuck.
+    Strip steak corned beef hamburger shoulder filet mignon brisket doner pork loin ball tip, chicken meatloaf.</p>
+  <p>Meatball burgdoggen corned beef t-bone shankle bacon. 
+  Burgdoggen landjaeger pork belly turducken meatloaf filet mignon ham hock boudin prosciutto. 
+  Pork belly turducken chuck sirloin hamburger tenderloin. Kielbasa ham hock tri-tip alcatra rump,
+   strip steak t-bone pork chop swine prosciutto ham tail. Beef sirloin shoulder, turkey prosciutto kevin brisket. 
+   Biltong shankle meatloaf, jerky doner bresaola t-bone corned beef burgdoggen pork belly jowl filet mignon.
+    Ball tip tri-tip meatball short loin pastrami.</p>
+</article>
+
+<footer>Copyright © DreamTeam</footer>
+</div>
+		
 		');
 
 include('scripts/navbar.php');
