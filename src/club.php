f3b15360718f9bafab7f9f7a585a3646e67acc05
Jack Neilson <jackdneilson@gmail.com>
2016-11-29 11:50:33 +0000

    Started implementing club.php

--- src/club.php
+++ src/club.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="utf-8">
     <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="../src/css/club.css"/>
-    <link rel="stylesheet" type="text/css" href="../src/css/navbar.css"/>
-    <link rel="stylesheet" type="text/css" href="../src/css/general.css"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo($_SERVER["SERVER_NAME"] . "/src/css/navbar.css");?>"
+    <link rel="stylesheet" type="text/css" href="http://go-portlethen.azurewebsites.net/src/css/club.css"/>
+    <link rel="stylesheet" type="text/css" href="http://go-portlethen.azurewebsites.net/src/css/navbar.css"/>
+    <link rel="stylesheet" type="text/css" href="http://go-portlethen.azurewebsites.net/src/css/general.css"/>
 </head>
 
 <body>
