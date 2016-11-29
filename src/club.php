ffb2ad405fba4801abead75691d20d939de21735
Jack Neilson <jackdneilson@gmail.com>
2016-11-29 11:52:03 +0000

    Started implementing club.php

--- src/club.php
+++ src/club.php
@@ -16,13 +16,14 @@
     <?php
     //TODO: Remove testing code
     include("scripts/db_connect_test.php");
+    echo($params["club_id"]);
     $sql = $db->prepare("SELECT Club.name, Club.genre, Club.description, Club.contact_info FROM Club WHERE club_id = ?");
     $club_id = mysqli_real_escape_string($params["club_id"]);
     $sql->bind_param("s", $club_id);
     $results = $sql->execute();
     $row = $results->fetch_array();
 
-    if(!is_null($row["name"]) && isset($row["name"])) {
+    if(!(is_null($row["name"])) && isset($row["name"])) {
         echo("
             {$row["name"]}
         ");
