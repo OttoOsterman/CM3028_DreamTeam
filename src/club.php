4b8228eda2098af8ca1b89c4dc55ddb1e9a5a20a
Jack Neilson <jackdneilson@gmail.com>
2016-12-05 12:20:05 +0000

    Refactored club page

--- src/club.php
+++ src/club.php
@@ -2,7 +2,23 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Home</title>
+    <?php
+    include('scripts/db_connect_test.php');
+    $request_array = explode("/", $_SERVER["REQUEST_URI"]);
+    $club_id = end($request_array);
+    $sql = "SELECT Club.name, Club.genre, Club.description, Club.contact_info FROM Club WHERE club_id = " . mysqli_real_escape_string($db, $club_id);
+    $results = $db->query($sql);
+    $row = $results->fetch_array();
+    ?>
+    <title>
+        <?php
+        if (isset($row['name'])) {
+            echo("{$row['name']}");
+        } else {
+            echo("Not found");
+        }
+        ?>
+    </title>
     <link rel="stylesheet" type="text/css" href="http://go-portlethen.azurewebsites.net/src/css/club.css"/>
     <link rel="stylesheet" type="text/css" href="http://go-portlethen.azurewebsites.net/src/css/navbar.css"/>
     <link rel="stylesheet" type="text/css" href="http://go-portlethen.azurewebsites.net/src/css/general.css"/>
@@ -12,14 +28,6 @@
     <?php include('scripts/navbar.php') ?>
 <div id="clubContainer">
     <?php
-    //TODO: Remove testing code
-    include('scripts/db_connect_test.php');
-    $request_array = explode("/", $_SERVER["REQUEST_URI"]);
-    $club_id = end($request_array);
-    $sql = "SELECT Club.name, Club.genre, Club.description, Club.contact_info FROM Club WHERE club_id = " . mysqli_real_escape_string($db, $club_id);
-    $results = $db->query($sql);
-    $row = $results->fetch_array();
-
     if(isset($row["name"]) && $row["name"] !== null) {
         echo("
         <section class='clubSection'>
