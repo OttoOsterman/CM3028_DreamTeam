abd1d49270c7f5b5874332b0a647d24b31bdf950
OttoOsterman <ottosterman@gmail.com>
2016-12-05 17:05:41 +0000

    Added code for news articles

--- src/health_wellbeing.php
+++ src/health_wellbeing.php
@@ -3,16 +3,20 @@
 	<link rel="stylesheet" type="text/css" href="../src/css/navbar.css"/>
 	<link rel="stylesheet" type="text/css" href="../src/css/general.css"/>
 
-    <?php include('scripts/db_connect_test.php') ?>
+    <?php
+    include('scripts/db_connect_test.php');
+    $request_array = explode("/", $_SERVER["REQUEST_URI"]);
+    $club_id = end($request_array);
+    $sql = "SELECT News.news_id, News.title, News.content, News.date FROM News";
+    $result = $db->query($sql);
+    $row = $results->fetch_array();
+    ?>
 </head>
 <body>
 
 <?php include('scripts/navbar.php'); ?>
 
 <?php
-$sql = "SELECT News.news_id, News.title, News.content, News.date FROM News";
-$result = $db->query($sql);
-
 echo("
             <section class='newsSection'>
                 <section class='newsTitle'>{$row['title']}</section>
