52e4b288eb0f97f5ce2e20b2bc724600d6aededc
OttoOsterman <ottosterman@gmail.com>
2016-12-05 15:28:15 +0000

    Added Sportlethen Facebook timeline feed to Clubs and Societies page.

--- src/clubs_societies.php
+++ src/clubs_societies.php
@@ -9,6 +9,14 @@
 </head>
 <body>
 <?php include('scripts/navbar.php') ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=985300261517338";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 <div id=clublist>
 	<?php
 	$sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id";
@@ -37,5 +45,11 @@
         }
 	}
 	?>
+    <div class="fb-page" data-href="https://www.facebook.com/Sportlethen"
+         data-tabs="timeline" data-width="400" data-height="600" data-small-header="false"
+         data-adapt-container-width="true" data-hide-cover="false"
+         data-show-facepile="true">
+        <blockquote cite="https://www.facebook.com/Sportlethen" class="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/Sportlethen">Sportlethen CSH</a></blockquote></div>
 </div>
 </body>
