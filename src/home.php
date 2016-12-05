5b5cb1a69e79356e1652505eee0bf429e3a13acf
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-05 18:36:31 +0000

    banner

--- src/home.php
+++ src/home.php
@@ -15,48 +15,5 @@
 			
 </body>
 
-<!--image header-->
-<img src="/src/images/go-portlethen.jpg"/>
-
-
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-
-<div class="images">
-
-	<img class="mySlides" src="src/images/downies.jpg" style="width:100%">
-	<!--
-	<img class="mySlides" src=".../src/image" style="width:100%">
-	<img class="mySlides" src="img_mountains.jpg" style="width:100%">
-	<img class="mySlides" src="img_forest.jpg" style="width:100%">
--->
-	<a class="w3-btn-floating w3-display-left" onclick="plusDivs(-1)">&#10094;</a>
-	<a class="w3-btn-floating w3-display-right" onclick="plusDivs(1)">&#10095;</a>
-</div>
-
-<script>
-	var slideIndex = 1;
-	showDivs(slideIndex);
-
-	function plusDivs(n) {
-		showDivs(slideIndex += n);
-	}
-
-	function showDivs(n) {
-		var i;
-		var x = document.getElementsByClassName("mySlides");
-		if (n > x.length) {slideIndex = 1}
-		if (n < 1) {slideIndex = x.length}
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "none";
-		}
-		x[slideIndex-1].style.display = "block";
-	}
-</script>
-
-</body>
-</html>
-
-
-
 
 </html>
