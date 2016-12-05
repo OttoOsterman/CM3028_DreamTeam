fa52ef4b8eed508aa1ede868068b2af0bf3a01f4
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-05 15:38:12 +0000

    image slider

--- src/home.php
+++ src/home.php
@@ -15,5 +15,52 @@
 			Welcome to Go Portlethen
 		</h1>
 	</div>
+		<!-- Navigation Slider -->
+		<div class="navSlider">
+			<ul>
+				<li>
+					<div class="about green slide">
+						<a href="about.html">About Us</a>
+					</div>
+					<a href="about.html">
+						<img src="images/hillside.jpg" alt="Group Photo">
+					</a>
+				</li>
+				<li>
+					<div class="yellow slide">
+						<a href="volunteer.html">Volunteer</a>
+					</div>
+					<a href="volunteer.html">
+						<img src="images/FishersOnCoast.jpg" alt="Child placing ball">
+					</a>
+				</li>
+				<!--
+				<li>
+					<div class="blue slide">
+						<a href="news.html">News</a>
+					</div>
+					<a href="news.html">
+						<img src="img/RealHeroesCrop.jpg" alt="Winners of RBS Real Heroes 2014">
+					</a>
+				</li>
+				<li>
+					<div class="pink slide">
+						<a href="gallery.html">Gallery</a>
+					</div>
+					<a href="gallery.html">
+						<img src="img/HorseWalkCrop.jpg" alt="RDA Horse walkies">
+					</a>
+				</li>
+				<li>
+					<div class="purple slide">
+						<a href="contact.html">Contact Us</a>
+					</div>
+					<a href="contact">
+						<img src="img/AwardCrop.jpg" alt="Receiving RBS Real Heroes Award">
+					</a>
+				</li>
+				-->
+			</ul>
+		</div>
 </body>
 </html>
