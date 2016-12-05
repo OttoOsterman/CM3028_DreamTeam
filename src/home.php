e3874266d309d298450df8f30dc82150fb33cbd9
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-05 17:06:43 +0000

    image slider

--- src/home.php
+++ src/home.php
@@ -24,11 +24,8 @@
 		</div>
 		<div id="sliderFrame">
 			<div id="slider">
-				<a href="http://www.menucool.com/javascript-image-slider" target="_blank">
-					<img src="images/image-slider-1.jpg" alt="Welcome to Menucool.com" />
-				</a>
-				<img src="../src/images/FishersOnCoast.jpg" />
-				<img src="images/hillside.jpg" alt="" />
+				<img src="../src/images/beach%20image.jpg" />
+				<img src="images/downies.jpg" alt="" />
 				<!--
 				<img src="images/image-slider-4.jpg" alt="#htmlcaption" />
 				<img src="images/image-slider-5.jpg" />
