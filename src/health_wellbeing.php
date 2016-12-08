2c5c7ce69afa083430c77f7a7df740927cadcfc5
OttoOsterman <ottosterman@gmail.com>
2016-12-08 20:16:04 +0000

    PHP for events datetime check

--- src/health_wellbeing.php
+++ src/health_wellbeing.php
@@ -102,7 +102,7 @@
         <h1 class="sectionHeader">Events</h1>
 
         <?php
-        $sql = "SELECT Event.event_id, Event.name, Event.description, Event.date FROM Event WHERE Event.date > {time() - 604800}";
+        $sql = "SELECT Event.event_id, Event.name, Event.description, Event.date FROM Event WHERE Event.date > {DATE - 604800}";
         $result = $db->query($sql);
 
         while ($row = $result->fetch_array()) {
