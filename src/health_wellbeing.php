55202dddbc1f7e125611f6acd0fd9ddde272754e
OttoOsterman <ottosterman@gmail.com>
2016-12-08 18:06:27 +0000

    HTML + CSS for health and wellbeing.

--- src/health_wellbeing.php
+++ src/health_wellbeing.php
@@ -15,7 +15,7 @@
 </div>
 
 <!-- Static information about health and wellbeing -->
-<div id="healthSection">
+<div id="staticHealthSection">
     <!-- Container for a static information article -->
     <div class="staticInfoContainer">
         <!-- Header for a static information article -->
@@ -37,8 +37,29 @@
         aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
     </div>
     <div class="staticInfoContainer">
+        <!-- Header for a static information article -->
         <h1 class="staticInfoHeader">
-            Annual Bacon Eating Contest 20.1.2017
+            Annual Bacon Eating Contest 2016
+        </h1>
+        Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
+        porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia laborum
+        in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
+
+        Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
+        sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
+        mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter. Shankle
+        aliquip doner, brisket in enim pork chop.
+
+        Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum ex.
+        Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
+        turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
+        aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
+    </div>
+    </div>
+<div id="dynamicNewsSection">
+    <div class="dynamicNewsContainer">
+        <h1 class="dynamicNewsHeader">
+            Can't Wait For Christmas Turkey
         </h1>
         Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
         porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia laborum
