5faffb0b25ac71c040583fd662b6c249e393ecc5
OttoOsterman <ottosterman@gmail.com>
2016-12-08 18:35:35 +0000

    PHP for getting news articles from database

--- src/health_wellbeing.php
+++ src/health_wellbeing.php
@@ -4,6 +4,9 @@
     <link rel="stylesheet" type="text/css" href="../src/css/navbar.css"/>
     <link rel="stylesheet" type="text/css" href="../src/css/general.css"/>
     <link rel="stylesheet" type="text/css" href="../src/css/health_wellbeing.css"/>
+
+    <!-- Connect database -->
+    <?php include('scripts/db_connect_test.php') ?>
 </head>
 <body>
 
@@ -14,69 +17,103 @@
 
 </div>
 
+<!-- Container for health and wellbeing content -->
 <div id="healthWellbeing">
-<!-- Static information about health and wellbeing -->
-<div id="staticHealthSection">
-    <!-- Container for a static information article -->
-    <div class="staticInfoContainer">
-        <!-- Header for a static information article -->
-        <h1 class="staticInfoHeader">
-            Bacon Is Good For You
-        </h1>
-        Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
-        porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia laborum
-        in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
 
-        Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
-        sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
-        mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter. Shankle
-        aliquip doner, brisket in enim pork chop.
+    <!-- Static information about health and wellbeing -->
+    <div id="staticHealthSection">
+        <!-- Container for a static information article -->
+        <div class="staticInfoContainer">
+            <!-- Header for a static information article -->
+            <h1 class="staticInfoHeader">
+                Bacon Is Good For You
+            </h1>
+            Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
+            porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia
+            laborum
+            in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
 
-        Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum ex.
-        Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
-        turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
-        aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
+            Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
+            sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
+            mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter.
+            Shankle
+            aliquip doner, brisket in enim pork chop.
+
+            Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum
+            ex.
+            Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
+            turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
+            aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
+        </div>
+        <div class="staticInfoContainer">
+            <!-- Header for a static information article -->
+            <h1 class="staticInfoHeader">
+                Annual Bacon Eating Contest 2016
+            </h1>
+            Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
+            porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia
+            laborum
+            in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
+
+            Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
+            sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
+            mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter.
+            Shankle
+            aliquip doner, brisket in enim pork chop.
+
+            Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum
+            ex.
+            Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
+            turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
+            aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
+        </div>
     </div>
-    <div class="staticInfoContainer">
-        <!-- Header for a static information article -->
-        <h1 class="staticInfoHeader">
-            Annual Bacon Eating Contest 2016
-        </h1>
-        Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
-        porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia laborum
-        in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
 
-        Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
-        sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
-        mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter. Shankle
-        aliquip doner, brisket in enim pork chop.
+    <!-- Dynamic news section that pulls news articles and events from the database -->
+    <div id="dynamicNewsSection">
+        <!-- Container for a single news/event article -->
+        <div class="dynamicNewsContainer">
+            <!-- Header for a single news/event article -->
+            <h1 class="dynamicNewsTitle">
+                Can't Wait For Christmas Turkey
+            </h1>
+            Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
+            porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia
+            laborum
+            in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
 
-        Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum ex.
-        Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
-        turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
-        aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
+            Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
+            sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
+            mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter.
+            Shankle
+            aliquip doner, brisket in enim pork chop.
+
+            Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum
+            ex.
+            Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
+            turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
+            aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
+        </div>
+
+
+        <?php
+        $sql = "SELECT News.news_id, News.title, News.content, News.date FROM News";
+        $result = $db->query($sql);
+
+        while ($row = $result->fetch_array()) {
+                echo("
+            <div class='dynamicNewsContainer'>
+                <h1 class='dynamicNewsTitle'><a href='news/{$row['news_id']}'>{$row['title']}</a></h1>
+                <div class='newsContent'>{$row['content']}</div>
+                <div class='newsDate'>{$row['date']}</div>
+            </div>
+			");
+        }
+        ?>
+
+
     </div>
-    </div>
-<div id="dynamicNewsSection">
-    <div class="dynamicNewsContainer">
-        <h1 class="dynamicNewsHeader">
-            Can't Wait For Christmas Turkey
-        </h1>
-        Spicy jalapeno bacon ipsum dolor amet ham doner labore dolor veniam flank tempor. Deserunt burgdoggen ad
-        porchetta sunt pastrami boudin sed magna corned beef nisi. Lorem pastrami ullamco jerky tongue officia laborum
-        in tempor beef ribs magna turkey. Lorem dolore incididunt, leberkas filet mignon swine shank.
 
-        Flank deserunt doner, velit t-bone landjaeger leberkas. Doner veniam strip steak, est turkey tempor proident
-        sint pork loin andouille voluptate. Tenderloin shoulder nulla burgdoggen ground round reprehenderit boudin
-        mollit cupim pancetta tri-tip consectetur filet mignon salami in. Kielbasa in ground round frankfurter. Shankle
-        aliquip doner, brisket in enim pork chop.
-
-        Strip steak beef ribs lorem aliqua leberkas short ribs commodo proident pork chop tenderloin esse laborum ex.
-        Salami frankfurter alcatra duis. Ex beef nulla proident eu dolore tenderloin beef ribs shank nostrud anim
-        turkey. Bacon fatback shank velit, ex proident tri-tip minim. Turkey cupim exercitation ut. Chuck spare ribs
-        aute excepteur id esse turducken proident short ribs cow commodo nostrud ham frankfurter ut.
-    </div>
-</div>
 </div>
 
 </body>
