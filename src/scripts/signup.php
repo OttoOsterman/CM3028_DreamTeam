58a718d4a50a43262825ff46b500d56f4ba3edc1
Jack Neilson <jackdneilson@gmail.com>
2016-12-08 17:37:26 +0000

    Added signup logic

---
+++ src/scripts/signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="./src/css/signup.css"/>
+	<link rel="stylesheet" type="text/css" href="./src/css/navbar.css"/>
+	<link rel="stylesheet" type="text/css" href="./src/css/general.css"/>
+</head>
+<?php
+include("navbar.php");
+if (isset($_SESSION["username"])) {
+    echo("
+        <body>
+        <h1>Please log out before attempting to create a new account.</h1>
+        </body>
+    ");
+} else {
+    echo("
+        <div id=\"signup_form\">
+            <form action='signup.php'>
+                <label>Please enter your e-mail address</label>
+                <input type='text' id='username' required>
+                <label>Please enter your password</label>
+                <input type='password' id='password' required>
+                <button type='submit'>Submit</button>
+            </form>
+        ");
+    if($POST) {
+        //TODO: REMOVE TESTING CODE
+        include("db_connect_test.php");
+        $username = $_POST["username"];
+        $username = stripslashes($username);
+        $username = mysqli_real_escape_string($username);
+
+        $getusername = $db->prepare("SELECT username FROM User WHERE username = ?");
+        $getusername->bind_param("s", $username);
+        $getusername->execute();
+        $getusername->bind_result($result);
+        $getusername->fetch();
+
+        echo("Result is " . $result);
+    }
+}
+?>
