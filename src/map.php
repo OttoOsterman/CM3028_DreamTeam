76d9a6f7afa966283332d9ac108108c0f410da52
Jack Neilson <jackdneilson@gmail.com>
2016-12-08 19:50:41 +0000

    Added code to show info window

--- src/map.php
+++ src/map.php
@@ -8,6 +8,8 @@
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKI13n7BTCWoJj841jA6OielBD8bRbg6M" type="text/JavaScript"></script>
 	
 	<script type="text/JavaScript">
+		var infoWindow = new google.maps.InfoWindow;
+
 		function downloadUrl(url,callback) {
 			var request = window.ActiveXObject ?
 				new ActiveXObject('Microsoft.XMLHTTP') :
@@ -21,6 +23,13 @@
 
 			request.open('GET', url, true);
 			request.send(null);
+		}
+
+		function bindInfoWindow(marker, map, infoWindow, html) {
+			google.maps.event.addListener(marker, 'click', function() {
+				infoWindow.setContent(html);
+				infoWindow.open(map, marker);
+			});
 		}
 
 		function load() {
@@ -37,11 +46,14 @@
 					var point = new google.maps.LatLng(
 						parseFloat(markers[i].getAttribute("lat")),
 						parseFloat(markers[i].getAttribute("lng"))
-					)
+					);
+					var name = markers[i].getAttribute("name");
+					var html = "<b>" + name + "</b>";
 					var marker = new google.maps.Marker({
 						map: map,
 						position: point
 					});
+					bindInfoWindow(marker, map, infoWindow, html);
 				}
 			});
 		}
