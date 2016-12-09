0ea73f1ca48c5258a8f8566a04f638c401bd0ff2
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-09 20:16:14 +0000

    image slider

--- src/home.php
+++ src/home.php
@@ -40,7 +40,7 @@
 	<img src="/src/images/go-portlethen.jpg" style="width:100%";/>
 </div>
 
-<div id="container">/
+<div id="container">
 	<img src="/src/imageSlider/img1.jpg" id="img"/>
 	<div id="left_arrow"> <img onClick+"slide(-1)"class="left" src="/src/imageSlider/arrow_left.png"/></div>
 	<div id="right_arrow"> <img onClick+"slide(1)" class="right" src="/src/imageSlider/arrow_right.png"/></div>
