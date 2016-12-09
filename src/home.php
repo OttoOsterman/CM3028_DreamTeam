fc096f949896cad4a8b9e33ddea39b08d3f7b11d
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-09 20:27:33 +0000

    image slider

--- src/home.php
+++ src/home.php
@@ -24,9 +24,8 @@
 
 <div id="container">
 	<img src="/src/imageSlider/img1.jpg" id="img"/>
-	<div id="left_arrow"> <img onClick+"slide(-1)"class="left" src="/src/imageSlider/arrow_left.png"/></div>
-	<div id="right_arrow"> <img onClick+"slide(1)" class="right" src="/src/imageSlider/arrow_right.png"/></div>
-	
+	<div id="left_arrow"> <img onClick="slide(-1)" id="left" src="/src/imageSlider/arrow_left.png"/></div>
+	<div id="right_arrow"> <img onClick="slide(1)" id="right" src="/src/imageSlider/arrow_right.png"/></div>
 </div>
 
 
