f89f1451f391fa6d5b8493d668be6c4ac63dae86
Andrew <admac21@gmail.com>
2016-12-10 19:35:51 +0000

    Popup login box

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -79,7 +79,40 @@ echo('
 	            <h2>Here i am</h2>
 	            <a class="close" href="#">x</a>
 	            <div class="content">
-	                This is the popup content ;)
+	                <!-- Placeholder login form, REMOVE ASAP -->
+<form action="javascript:return login()">
+	<label>E-mail address</label>
+	<input type="text" id="username" required>
+	<label>Password</label>
+	<input type="password" id="password" required>
+	<input type="submit" onclick="login()" value="Log in">
+	<?php
+	if (isset($_SESSION{"error"}) && !(is_null($_SESSION["error"]))) {
+		if($_SESSION["error"] == "username_not_found") {
+			echo("<label>The username entered wasn\'t found, please try again</label>");
+		} elseif ($_SESSION["error"] == "wrong_password") {
+			echo("<label>The password entered didn\'t match the username, please try again</label>");
+		}
+	}
+	?>
+</form>
+
+<script>
+	function login() {
+		var username = document.getElementById("username").value;
+		var password = document.getElementById("password").value;
+		var args = "username=" + username + "&password=" + password;
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			if (req.readyState == XMLHttpRequest.DONE) {
+				location.reload();
+			}
+		};
+		req.open("POST", "https://go-portlethen.azurewebsites.net/login");
+		req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		req.send(args);
+	}
+</script>
                 </div>
             </div>
 	    
