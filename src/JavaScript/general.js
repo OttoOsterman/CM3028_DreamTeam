209e7578023a80b275031c886c23a97179f78204
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-10 15:13:33 +0000

    image slider

--- src/JavaScript/general.js
+++ src/JavaScript/general.js
@@ -1,17 +1,1 @@
-var imageCount = 1;
-var total = 2;
 
-function slide(x) {
-    var Image = document.getElementById('img');
-    Image.src = "./src/imageSlider/img" + imageCount + ".jpg";
-
-    imageCount + 1;
-
-    if (imageCount > total) {
-        imageCount = 1;
-    }
-    if (imageCount < 1){
-        imageCount = total;
-    }
-    
-}
