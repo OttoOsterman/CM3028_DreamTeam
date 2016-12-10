f6386a9927b667c81c8d06a2d0047b874005a82a
Andrew <admac21@gmail.com>
2016-12-10 22:47:06 +0000

    More work on login popup

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -89,6 +89,32 @@ echo('
 	                <label>Password</label>
 	                <input type="password" id="password" required>
 	                <input type="submit" onclick="login()" value="Log in">
+	                </form>
+	                ');
+	                if (isset($_SESSION{"error"}) && !(is_null($_SESSION["error"]))) {
+		                if($_SESSION["error"] == "username_not_found") {
+			                echo("<label>The username entered wasn\\'t found, please try again </label > ");
+		                } elseif ($_SESSION["error"] == "wrong_password") {
+			                echo(" < label>The password entered didn\\'t match the username, please try again</label>");
+		                }
+	                }
+                    echo('
+<script>
+	function login() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        var args = "username=" + username + "&password=" + password;
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+            if (req.readyState == XMLHttpRequest.DONE) {
+                location.reload();
+            }
+        };
+        req.open("POST", "https://go-portlethen.azurewebsites.net/login");
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        req.send(args);
+    }
+</script>
 	            </div>
 	        </div>>
 	  </div>          
