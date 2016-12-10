dee2c1740a4bfeef6958e9bc0db35d3a90079631
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-10 16:54:56 +0000

    center

--- src/home.php
+++ src/home.php
@@ -8,7 +8,7 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<title>Home</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
 	<link rel="stylesheet" type="text/css" href="./src/css/home.css"/>
 	<link rel="stylesheet" type="text/css" href="./src/css/navbar.css"/>
 	<link rel="stylesheet" type="text/css" href="./src/css/general.css"/>
@@ -63,6 +63,7 @@ echo("Session error value is: " . $_SESSION["error"]);
 
     <!--Image Slider-->
 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <div class="w3-content w3-display-container">
         <img class="mySlides" src=".\src\imageSlider\img1.jpg" style="width:100%">
         <img class="mySlides" src=".\src\imageSlider\beachSunset3.jpg" style="width:100%">
@@ -97,12 +98,6 @@ echo("Session error value is: " . $_SESSION["error"]);
 
 
 		<!--text-->
-
-
-
-
-
-
 
 
 
