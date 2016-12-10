17a3bb20b624f79451f86cbc389a7f8ec9343019
Andrew <admac21@gmail.com>
2016-12-10 18:53:03 +0000

    Fixes for experimental nav bar

--- src/home.php
+++ src/home.php
@@ -22,9 +22,74 @@ session_start();
 <body>
 
 <div class="pageWidth">
+
+	<section id="myPopup" class="popup">
+		<!-- Popup content -->
+		<section class="popup-content">
+			<span class="close">X</span>
+			<!--Google login button-->
+			<section id="customBtn" class="customGPlusSignIn" data-onsuccess="onSignIn" onclick="login1()">
+				<span class="icon"></span>
+				<span class="buttonText">Google</span>
+			</section>
+			<section id="name"></section>
+			<script>startApp();</script>
+			<script>
+				//Login function
+				function login1() {
+					if ($('.login').text('Login')) {
+						$('.login').text('Logout')
+					}
+				}
+			</script>
+			<!--Facebook login-->
+			<section id="fb-root"></section>
+			<script>(function (d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s);
+					js.id = id;
+					js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.6";
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+			<section class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false"
+					 data-auto-logout-link="true"></section>
+			<!--Logout button-->
+			<a href="#" class="logout" onclick="logout1()">Logout</a>
+			<script>
+				//Signout function
+				function signOut2() {
+					FB.logout();
+// user is now logged out of Facebook
+					console.log('User signed out of Facebook');
+				}
+
+			</script>
+			<!--Google sign out function-->
+			<script>
+				function signOut1() {
+					var auth2 = gapi.auth2.getAuthInstance();
+					auth2.signOut().then(function () {
+// user is now logged out of Google+
+						console.log('User signed out of Google+.');
+					});
+					$('#name').text('Signed out.')
+				}
+			</script>
+			<script>
+				//Logout function
+				function logout1() {
+					signOut1();
+					signOut2();
+					$('.login').text('Login')
+				}
+			</script>
+		</section>
+	</section>
+
 <!-- navigation bar-->
 		<?php include('scripts/navbar.php') ?>
-
+<a id="loginBtn" class="login" href="#">Login</a>
 <!-- logo -->
 
 		<img id ="banner" src="/src/images/go-portlethen.jpg" ;/>
@@ -57,7 +122,7 @@ session_start();
 			if (req.readyState == XMLHttpRequest.DONE) {
 				location.reload();
 			}
-		}
+		};
 		req.open("POST", "https://go-portlethen.azurewebsites.net/login");
 		req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		req.send(args);
