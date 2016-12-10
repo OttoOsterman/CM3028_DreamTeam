6645bc76ca68bf5def44531203de79022e1b870c
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-10 12:45:39 +0000

    image slider

--- src/home.php
+++ src/home.php
@@ -58,19 +58,19 @@ session_start();
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img src="imageSlider/img1.jpg" alt="insert alt" width="460" height="345">
+					<img src="src/imageSlider/img1.jpg" alt="insert alt" width="460" height="345">
 				</div>
 
 				<div class="item">
-					<img src="imageSlider/img2.jpg" alt="insert alt" width="460" height="345">
+					<img src="src/imageSlider/img2.jpg" alt="insert alt" width="460" height="345">
 				</div>
 
 				<div class="item">
-					<img src="imageSlider/img3.jpg" alt="insert alt" width="460" height="345">
+					<img src="src/imageSlider/img3.jpg" alt="insert alt" width="460" height="345">
 				</div>
 
 				<div class="item">
-					<img src="imageSlider/img1.jpg" alt="insert alt" width="460" height="345">
+					<img src="src/imageSlider/img1.jpg" alt="insert alt" width="460" height="345">
 				</div>
 			</div>
 
