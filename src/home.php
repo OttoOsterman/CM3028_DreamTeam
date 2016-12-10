daaed23b572f62865b1c5bad2d89158d6890b1a3
HarryNicholls <harry.nicholls@hotmail.com>
2016-12-10 14:36:56 +0000

    image slider

--- src/home.php
+++ src/home.php
@@ -16,29 +16,7 @@ session_start();
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 
     
-    <script>
-        var slideIndex = 1;
-        showDivs(slideIndex);
 
-        function plusDivs(n) {
-            showDivs(slideIndex += n);
-        }
-
-        function showDivs(n) {
-            var i;
-            var x = document.getElementsByClassName("mySlides");
-            if (n > x.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = x.length}
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
-            x[slideIndex-1].style.display = "block";
-        }
-
-        
-    </script>
-    
-    
 </head>
 
 <body>
@@ -97,8 +75,30 @@ echo("Session error value is: " . $_SESSION["error"]);
 
     </div>
 
-	
+	<script>
+		var slideIndex = 1;
+		showDivs(slideIndex);
 
+		function plusDivs(n) {
+			showDivs(slideIndex += n);
+		}
+
+		function showDivs(n) {
+			var i;
+			var x = document.getElementsByClassName("mySlides");
+			if (n > x.length) {slideIndex = 1}
+			if (n < 1) {slideIndex = x.length}
+			for (i = 0; i < x.length; i++) {
+				x[i].style.display = "none";
+			}
+			x[slideIndex-1].style.display = "block";
+		}
+
+
+	</script>
+
+
+		<!--Image Slider CLOSED-->
 
 
 
