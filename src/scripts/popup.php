c135421ab90d0b6b55d0761ff310c4ed0c6444f4
Andrew <admac21@gmail.com>
2016-12-10 22:55:39 +0000

    More work on login popup

--- src/scripts/popup.php
+++ src/scripts/popup.php
@@ -1,16 +1,27 @@
 <?php
-
-	                if (isset($_SESSION{"error"}) && !(is_null($_SESSION["error"]))) {
-		                if($_SESSION["error"] == "username_not_found") {
-			                echo("<label>The username entered wasn\'t found, please try again</label>");
-		                } elseif ($_SESSION["error"] == "wrong_password") {
-			                echo("<label>The password entered didn\'t match the username, please try again</label>");
-		                }
-	                }
-
 echo('
-                    </form>
-                    <script>
+<div id="popup1" class="overlay">
+	        <div class="popup">
+	            <h2>Please Login</h2>
+	            <a class="close" href="#">x</a>
+	            <div class="content">
+	                <form action="javascript:return login()">
+	                <label>E-mail address</label>
+	                <input type="text" id="username" required>
+	                <label>Password</label>
+	                <input type="password" id="password" required>
+	                <input type="submit" onclick="login()" value="Log in">
+	                </form>
+	                ');
+if (isset($_SESSION{"error"}) && !(is_null($_SESSION["error"]))) {
+    if($_SESSION["error"] == "username_not_found") {
+        echo("<label>The username entered wasn\\'t found, please try again </label > ");
+    } elseif ($_SESSION["error"] == "wrong_password") {
+        echo(" < label>The password entered didn\\'t match the username, please try again</label>");
+    }
+}
+echo('
+<script>
 	function login() {
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
@@ -26,9 +37,8 @@ echo('
         req.send(args);
     }
 </script>
-                </div>
-            </div>
-	    
-        </div>
-');
+	            </div>
+	        </div>>
+	  </div>          
+    ');
     ?>
