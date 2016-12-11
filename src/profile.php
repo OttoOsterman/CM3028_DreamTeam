a52952fd3c39e1da2fb3d8dd7054dd55df1cb1ca
Jack Neilson <jackdneilson@gmail.com>
2016-12-11 00:38:15 +0000

    Added edit button

--- src/profile.php
+++ src/profile.php
@@ -21,29 +21,52 @@ include ("scripts/db_connect_test.php");
 
 if ($_SESSION["acc_type"] == "admin") {
     $sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path, Photo.is_profile_photo FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id;";
-    $result = $db->query($sql);
+} else {
+    $get_clubs_sql = "SELECT club_id FROM ClubMember WHERE user_id = {$_SESSION['user_id']}";
+    $get_clubs_result = $db->query($get_clubs_sql);
+    if ($get_clubs_result->num_rows > 0) {
+        $row = $get_clubs_result->fetch_array();
+        $sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path, Photo.is_profile_photo FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id WHERE (Club.club_id = {$row['club_id']}";
+        while ($row = $get_clubs_result->fetch_array()) {
+            $sql = $sql . " OR Club.club_id = '{$row["club_id"]}'";
+        }
+        $sql = $sql . ");";
+    }
+}
+$result = $db->query($sql);
+if ($result->num_rows > 0) {
     while ($row = $result->fetch_array()) {
         if (isset($row['photo_path']) && $row['is_profile_photo'] == '1') {
             echo("
-            <section class='clubSection'>
-                <img class='clubImage' src={$row['photo_path']}>
-                <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
-                <div class='clubGenre'>{$row['genre']}</div>
-                <div class='clubDesc'>{$row['description']}</div>
-            </section>
-			");
+        <section class='clubSection'>
+            <img class='clubImage' src={$row['photo_path']}>
+            <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
+            <div class='clubGenre'>{$row['genre']}</div>
+            <form action='add_club' method='POST'>
+                <input type='hidden' name='club_id' value='{$row["club_id"]}'
+                <input type='submit' value='Edit'>
+            </form>
+        </section>
+		");
         } else if (!(isset($row['photo_path']))) {
             echo("
-			<section class='clubSection'>
-			    <img class='clubImage' src='../src/images/placeholder.png'>
-			    <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
-			    <div class='clubGenre'>{$row['genre']}</div>
-			    <div class='clubDesc'>{$row['description']}</div>
-			</section>
-            ");
+		<section class='clubSection'>
+		    <img class='clubImage' src='../src/images/placeholder.png'>
+		    <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
+		    <div class='clubGenre'>{$row['genre']}</div>
+		    <form action='add_club' method='POST'>
+                <input type='hidden' name='club_id' value='{$row["club_id"]}'
+                <input type='submit' value='Edit'>
+            </form>
+		</section>
+       ");
         }
-
     }
+} else {
+    echo ("<h2> Oops! It looks like you're not a member of any clubs yet.</h2>");
+}
+
+if ($_SESSION["acc_type"] == "admin") {
     echo ("
     <form action='javascript:return add_club()'>
     <label>Club Name: </label>
@@ -66,42 +89,6 @@ if ($_SESSION["acc_type"] == "admin") {
     }
     </script>
     ");
-} else {
-    $get_clubs_sql = "SELECT club_id FROM ClubMember WHERE user_id = {$_SESSION['user_id']}";
-    $get_clubs_result = $db->query($get_clubs_sql);
-    if ($get_clubs_result->num_rows > 0) {
-        $row = $get_clubs_result->fetch_array();
-        $sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path, Photo.is_profile_photo FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id WHERE (Club.club_id = {$row['club_id']}";
-        while($row = $get_clubs_result->fetch_array()) {
-            $sql = $sql . " OR Club.club_id = '{$row["club_id"]}'";
-        }
-        $sql = $sql . ");";
-        $result = $db->query($sql);
-        while ($row = $result->fetch_array()) {
-            if (isset($row['photo_path']) && $row['is_profile_photo'] == '1') {
-                echo("
-            <section class='clubSection'>
-                <img class='clubImage' src={$row['photo_path']}>
-                <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
-                <div class='clubGenre'>{$row['genre']}</div>
-                <div class='clubDesc'>{$row['description']}</div>
-            </section>
-			");
-            } else if (!(isset($row['photo_path']))) {
-                echo("
-			<section class='clubSection'>
-			    <img class='clubImage' src='../src/images/placeholder.png'>
-			    <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
-			    <div class='clubGenre'>{$row['genre']}</div>
-			    <div class='clubDesc'>{$row['description']}</div>
-			</section>
-            ");
-            }
-        }
-    } else {
-        echo("<h1>Oops! It looks like you aren't a member of any clubs yet.</h1>");
-    }
 }
 ?>
-
 </body>
