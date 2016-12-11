b26c2e592fe4f643018262f0cd2a2aaf93f67600
Jack Neilson <jackdneilson@gmail.com>
2016-12-11 00:04:31 +0000

    Added code to show all clubs if user is an admin

--- src/profile.php
+++ src/profile.php
@@ -19,15 +19,8 @@ include ("scripts/navbar.php");
 //TODO: REMOVE TESTING CODE
 include ("scripts/db_connect_test.php");
 
-$get_clubs_sql = "SELECT club_id FROM ClubMember WHERE user_id = {$_SESSION['user_id']}";
-$get_clubs_result = $db->query($get_clubs_sql);
-if ($get_clubs_result->num_rows > 0) {
-    $row = $get_clubs_result->fetch_array();
-    $sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path, Photo.is_profile_photo FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id WHERE (Club.club_id = {$row['club_id']}";
-    while($row = $get_clubs_result->fetch_array()) {
-        $sql = $sql . " OR Club.club_id = '{$row["club_id"]}'";
-    }
-    $sql = $sql . ");";
+if ($_SESSION["acc_type"] == "admin") {
+    $sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path, Photo.is_profile_photo FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id;";
     $result = $db->query($sql);
     while ($row = $result->fetch_array()) {
         if (isset($row['photo_path']) && $row['is_profile_photo'] == '1') {
@@ -50,7 +43,43 @@ if ($get_clubs_result->num_rows > 0) {
             ");
         }
     }
+} else {
+    $get_clubs_sql = "SELECT club_id FROM ClubMember WHERE user_id = {$_SESSION['user_id']}";
+    $get_clubs_result = $db->query($get_clubs_sql);
+    if ($get_clubs_result->num_rows > 0) {
+        $row = $get_clubs_result->fetch_array();
+        $sql = "SELECT Club.club_id, Club.name, Club.genre, Club.description, Photo.photo_path, Photo.is_profile_photo FROM Club LEFT JOIN Photo ON Club.club_id = Photo.club_id WHERE (Club.club_id = {$row['club_id']}";
+        while($row = $get_clubs_result->fetch_array()) {
+            $sql = $sql . " OR Club.club_id = '{$row["club_id"]}'";
+        }
+        $sql = $sql . ");";
+        $result = $db->query($sql);
+        while ($row = $result->fetch_array()) {
+            if (isset($row['photo_path']) && $row['is_profile_photo'] == '1') {
+                echo("
+            <section class='clubSection'>
+                <img class='clubImage' src={$row['photo_path']}>
+                <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
+                <div class='clubGenre'>{$row['genre']}</div>
+                <div class='clubDesc'>{$row['description']}</div>
+            </section>
+			");
+            } else if (!(isset($row['photo_path']))) {
+                echo("
+			<section class='clubSection'>
+			    <img class='clubImage' src='../src/images/placeholder.png'>
+			    <h1 class='clubName'><a href='club/{$row['club_id']}'>{$row['name']}</a></h1>
+			    <div class='clubGenre'>{$row['genre']}</div>
+			    <div class='clubDesc'>{$row['description']}</div>
+			</section>
+            ");
+            }
+        }
+    } else {
+        echo("<h1>Oops! It looks like you aren't a member of any clubs yet.</h1>");
+    }
 }
+
 
 if ($_SESSION["acc_type"] == 'admin') {
     echo ("
