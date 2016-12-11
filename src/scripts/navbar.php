4e7e72788df7ee1d1381a8ed13b8c11dc3b07f0b
Andrew <admac21@gmail.com>
2016-12-11 17:58:09 +0000

    Popup box changes

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -82,17 +82,16 @@ echo('
 	        <div class="popup">
 	            <h2>Please Login</h2>
 	            <a class="close" href="#">x</a>
-	            <div class="content form-wrapper cf add">
-	                <div class="input-wrapper">
-	                    <form action="javascript:return login()">
-	                        <input class="text-box" placeholder="email address" type="text" id="username" required>
-	                        <input class="text-box" placeholder="password" type="password" id="password" required>
-	                    </form>
-	                </div>
-	                <div class="action-buttons">
-	                    <button class="greenButton button" type="submit" onclick="login()">Login</button>
-	                    <button class="closebutton button" href="#">Close</button>
-	                </div>
+	            <section class="content form-wrapper cf add">
+	                <form action="javascript:return login()">
+	                    <input class="text-box" placeholder="email address" type="text" id="username" required>
+	                    <input class="text-box" placeholder="password" type="password" id="password" required>
+	                </form>
+	            </section>
+	            <div class="action-buttons">
+	                <button class="greenButton button" type="submit" onclick="login()">Login</button>
+	                <button class="closebutton button" href="#">Close</button>
+	            </div>
 	                ');
 	                if (isset($_SESSION{"error"}) && !(is_null($_SESSION["error"]))) {
 		                if($_SESSION["error"] == "username_not_found") {
