c9b086a67dbb2e6ed23b2a5b299c3a14db37c0d5
Andrew <admac21@gmail.com>
2016-12-11 20:13:15 +0000

    Layout fixes for clubs/societies page

--- src/scripts/navbar.php
+++ src/scripts/navbar.php
@@ -120,6 +120,7 @@ echo('
 	        </div>
 	  </div>          
     ');
+            //Signup Pop-up
             echo('
 <div id="popup2" class="overlay">
 	        <div class="popup">
@@ -136,40 +137,43 @@ echo('
                 echo('
                 <h2>Create Account</h2>
 	            <section class="content form-wrapper">
-	                <form action="javascript:return login()">
-	                    <input class="text-box" placeholder="email address" type="text" id="username" required>
-	                    <input class="text-box" placeholder="password" type="password" id="password" required>
+	                <form id=\'signup_form\' action=\'signup\' method=\'POST\'>
+	                    <input class="text-box" placeholder="Please enter an e-mail address" type="text" id="username" required>
+	                    <input class="text-box" placeholder="Please enter a password" type="password" id="password" required>
 	                </form>
 	            <section class="action-buttons">
-	                <button class="greenButton button" type="submit" onclick="login()">Login</button>
+	                <button class="greenButton button" type="submit">Create</button>
 	                <a href="#"><button class="closebutton button">Close</button></a>
 	            </section>
 	            
 	                ');
-                if (isset($_SESSION{"error"}) && !(is_null($_SESSION["error"]))) {
-                    if ($_SESSION["error"] == "username_not_found") {
-                        echo("<label>The username entered wasn't found, please try again </label > ");
-                    } elseif ($_SESSION["error"] == "wrong_password") {
-                        echo("<label>The password entered didn't match the username, please try again</label>");
+                if($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    //TODO: REMOVE TESTING CODE
+                    include("scripts/db_connect_test.php");
+                    $username = $_POST["username"];
+                    $username = stripslashes($username);
+                    $username = mysqli_real_escape_string($db, $username);
+
+                    $sql = "SELECT username FROM User WHERE username = '{$username}'";
+                    $result = $db->query($sql);
+                    if ($result->num_rows > 0) {
+                        echo("<h2>Sorry, this e-mail is already in use.</h2>");
+                    } else {
+                        $password = $_POST["password"];
+                        $salt = date('U');
+                        $password_hash = hash('sha256', $password . $salt);
+                        $sql = "INSERT INTO User (username, hash, salt, acc_type) VALUES ('{$username}', '{$password_hash}', '{$salt}', 'general_user')";
+                        $db->query($sql);
+                        echo("
+				<script>
+					document.getElementById('signup_form').style.display = 'none';
+				</script>
+				<h2>Your account has been successfully created, please log in.</h2>
+				");
                     }
                 }
+
                 echo('
-<script>
-	function login() {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var args = "username=" + username + "&password=" + password;
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function() {
-            if (req.readyState == XMLHttpRequest.DONE) {
-                location.reload();
-            }
-        };
-        req.open("POST", "https://go-portlethen.azurewebsites.net/login");
-        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        req.send(args);
-    }
-</script>
 	            </section>
 	        </div>
 	  </div>          
