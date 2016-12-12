2e5735ec8904baecc233ba324a308b5ada06aab2
Jack Neilson <jackdneilson@gmail.com>
2016-12-12 21:14:53 +0000

    Changed prod db connect script to point at the right database

--- src/scripts/db_connect.php
+++ src/scripts/db_connect.php
@@ -3,7 +3,7 @@ $db = new mysqli(
 		"us-cdbr-azure-southcentral-f.cloudapp.net",
 		"be9b6ca39cad35",
 		"e7f30e0c",
-		"maps"
+		"cm3028dreamteam"
 		);
 if($db->connect_errno) {
 	die('Connectfailed['.$db->connect_error.']');
