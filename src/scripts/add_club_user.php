7b35927d7c889247c2628a821b1407f6612dde29
Jack Neilson <jackdneilson@gmail.com>
2016-12-12 16:07:34 +0000

    Added code to reload edit_club after adding a new user

--- src/scripts/add_club_user.php
+++ src/scripts/add_club_user.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-if (isset($_SESSION["acc_type"]) && isset($_SESSION["curr_club"]) && isset($_POST["username"])) {
+if (isset($_SESSION["acc_type"]) && isset($_POST["username"]) && isset($_POST["club_id"])) {
     if ($_SESSION["acc_type"] == "admin" || $_SESSION["acc_type"] == "club_admin") {
         //TODO: REMOVE RESTING CODE
         include("db_connect_test.php");
-        $sql = "SELECT user_id FROM User WHERE username = {$_POST["username"]}";
+        $sql = "SELECT user_id FROM User WHERE username = '{$_POST["username"]}'";
         $res = $db->query($sql);
         if ($res->num_rows == 1) {
             $sql = "INSERT INTO ClubMember (user_id, club_id) VALUES ({$row["user_id"]}, {$_SESSION{"curr_club"}})";
